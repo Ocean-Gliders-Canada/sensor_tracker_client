3a65290c1f6171b51326151fdaad2ee8da260fb3
xing ling <xing@TA020.WPA.Dal.Ca>
2018-05-09 11:06:57 -0300

    Initial commit

---
+++ sensor_tracker_api/instruments_config.py
@@ -0,0 +1,9 @@
+SENSOR_TRACKER_CONNECTOR = 'http://bugs.ocean.dal.ca/sensor_tracker/api/'
+
+LOGIN_URL = "http://bugs.ocean.dal.ca/sensor_tracker/admin/login/?next=/sensor_tracker/admin/"
+
+PAYLOAD = {
+    "username": "",
+    "password": "",
+    "csrfmiddlewaretoken": ""
+}
