4d92c7e5ed0bade67a1e064458cbfb07c8d0dd22
xiang <xn947387@dal.ca>
2018-06-14 17:31:29 +0100

    Delete instruments_config.py

--- sensor_tracker_api/instruments_config.py
+++
@@ -1,9 +0,0 @@
-SENSOR_TRACKER_CONNECTOR = 'http://bugs.ocean.dal.ca/sensor_tracker/api/'
-
-LOGIN_URL = "http://bugs.ocean.dal.ca/sensor_tracker/admin/login/?next=/sensor_tracker/admin/"
-
-PAYLOAD = {
-    "username": "",
-    "password": "",
-    "csrfmiddlewaretoken": ""
-}
